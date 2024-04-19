78c08d4a1d9c762a4c1a24cab5d52bd71ad024a7
Andrzej Olender <andrzej@olender.me>
2024-04-19 06:46:12 +0000

    test build

---
+++ sshd/args.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config 
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config 
+mkdir -p /var/run/sshd 
+cat /root/tmp/authorized_keys >> /root/.ssh/authorized_keys 
+cat /root/tmp/id_rsa > /root/.ssh/id_rsa 
+cat /root/tmp/gitconfig > /root/.gitconfig
+chmod 600 /root/.ssh/id_rsa 
+cat /root/tmp/id_rsa.pub > /root/.ssh/id_rsa.pub 

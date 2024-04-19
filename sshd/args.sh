24a95ef37bc10e952ddf2ace032d24eb09637ff0
Andrzej Olender <andrzej@olender.me>
2024-04-19 06:56:42 +0000

    refactor script sh

--- sshd/args.sh
+++ sshd/args.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
+# Enable root login over SSH
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config 
-sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config 
-mkdir -p /var/run/sshd 
-cat /root/tmp/authorized_keys >> /root/.ssh/authorized_keys 
-cat /root/tmp/id_rsa > /root/.ssh/id_rsa 
+
+# Enable public key authentication
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+
+# Disable password authentication
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+# Create directory for sshd runtime
+mkdir -p /var/run/sshd
+
+# Copy authorized_keys file
+cat /root/tmp/authorized_keys >> /root/.ssh/authorized_keys
+
+# Copy private key
+cat /root/tmp/id_rsa > /root/.ssh/id_rsa
+
+# Copy Git configuration
 cat /root/tmp/gitconfig > /root/.gitconfig
-chmod 600 /root/.ssh/id_rsa 
+
+# Set appropriate permissions for private key
+chmod 600 /root/.ssh/id_rsa
+
+# Copy public key
 cat /root/tmp/id_rsa.pub > /root/.ssh/id_rsa.pub
